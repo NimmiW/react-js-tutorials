7a7ca31582f17082fbf3a6e7b1238a576d0e9169
Nimmi Weeraddana <Nimmi Weeraddana>
2017-03-14 01:56:53 +0530

    tutorial #5 was followed

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -6,7 +6,7 @@ import Layout from "./components/Layout";
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);*/
 
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 
 const reducer = function(state, action) {
  if(action.type == 'INC'){
@@ -15,10 +15,33 @@ const reducer = function(state, action) {
  if(action.type == 'DEC'){
      return state - action.payload;
  }
+ if(action.type == 'E'){
+     return new Error("Error : " , action.payload);
+ }
  return state;
 }
 
-const store = createStore(reducer, 0);
+//logger is a middleware, npm packages are available, but this manual
+const logger = (store) => (next) => (action) => {
+    // this the middleware code, anithing even, action.type='DEC' :)
+    console.log('action fired ', action); 
+    //reducers will not execute unless you secify the next in the middleware
+    next(action); //reducer call
+}
+
+//error is another middleware
+const error = (store) => (next) => (action) => {
+    try {
+        next(action); // tries the reducer function
+    } catch(e) {
+        console.log('AHHHH I am in the catch clause : ', e);
+    }
+    
+}
+
+const middleware = applyMiddleware(logger,error); // add the middle ware list here
+
+const store = createStore(reducer, 0, middleware);
 
 store.subscribe(() => {
     console.log('store changed',store.getState())
@@ -28,3 +51,4 @@ store.dispatch({type: 'INC', payload:1}) //run through reducer
 store.dispatch({type: 'DEC', payload:1}) 
 store.dispatch({type: 'DEC', payload:100}) 
 store.dispatch({type: 'INC', payload:1}) 
+store.dispatch({type: 'E', payload:'Oops! This is an Error!'}) 
