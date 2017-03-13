53a09a62de620b22af2206d9c2606f4b4f835e65
Nimmi Weeraddana <Nimmi Weeraddana>
2017-03-14 00:55:37 +0530

    tutorial #3 was followed

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,7 +1,30 @@
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom";
 
 import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(<Layout/>, app);*/
+
+import { createStore } from "redux";
+
+const reducer = function(state, action) {
+ if(action.type == 'INC'){
+     return state + action.payload;
+ }
+ if(action.type == 'DEC'){
+     return state - action.payload;
+ }
+ return state;
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+    console.log('store changed',store.getState())
+})
+
+store.dispatch({type: 'INC', payload:1}) //run through reducer
+store.dispatch({type: 'DEC', payload:1}) 
+store.dispatch({type: 'DEC', payload:100}) 
+store.dispatch({type: 'INC', payload:1}) 
