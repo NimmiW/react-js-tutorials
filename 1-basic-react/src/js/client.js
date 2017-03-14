0f57f31352eb2b6cc77c03c4e58db76f3e0f5131
Nimmi Weeraddana <Nimmi Weeraddana>
2017-03-14 08:34:45 +0530

    tut6 was followed

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -10,6 +10,7 @@ import { applyMiddleware, createStore } from "redux";
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import axios from 'axios';
+//redux-promise substitutinf thunk or addtion to thunk
 
 const initialState = {
     fetching: false,
