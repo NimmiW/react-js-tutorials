331298f0d2d545fdaaa6ec5bc69f29f001c80987
Nimmi Rashinika <nimmirashinika@gmail.com>
2017-03-15 18:19:00 +0530

    tut7

--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -1,7 +1,9 @@
+
 import React from "react";
 import { connect } from 'react-redux';
 
 import { fetchUser } from '../actions/userActions'
+import { fetchTweets } from '../actions/tweetsActions'
 
 @connect((store) => {
   return {// whatever you return here will act a props
@@ -16,9 +18,71 @@ export default class Layout extends React.Component {
     this.props.dispatch(fetchUser());
   }
 
-  render() {
-    //this.props.dispatch(fetchUser()); //in tut4 branch store.dispatch({type: 'CHANGE_NAME', payload:'Nimmi'})
+  fetchTweets() {
+    this.props.dispatch(fetchTweets())
+  }
+
+/*  render() {
+    console.log(this.componentNimmi()); //in tut4 branch store.dispatch({type: 'CHANGE_NAME', payload:'Nimmi'})
     console.log(this.props);
     return null;
-  };
+  }; */
+
+  render() {
+    const { user, tweets } = this.props;
+
+    //if (!tweets.length) {
+    //  return <button onClick={this.fetchTweets.bind(this)}>load tweets</button>
+    //}
+
+    //const mappedTweets = tweets.map(tweet => <li>{tweet.text}</li>)
+
+    return <div>
+      <h1>{user.name}</h1>
+      <button onClick={this.componentNimmi.bind(this)}>load tweets</button>
+    </div>
+  }
 }
+
+
+/*
+import React from "react"
+import { connect } from "react-redux"
+
+import { fetchUser } from "../actions/userActions"
+import { fetchTweets } from "../actions/tweetsActions"
+
+@connect((store) => {
+  return {
+    user: store.user.user,
+    userFetched: store.user.fetched,
+    tweets: store.tweets.tweets,
+  };
+})
+export default class Layout extends React.Component {
+  componentWillMount() {
+    this.props.dispatch(fetchUser())
+  }
+
+  fetchTweets() {
+    this.props.dispatch(fetchTweets())
+  }
+
+  render() {
+    const { user, tweets } = this.props;
+
+    if (!tweets.length) {
+      return <button onClick={this.fetchTweets.bind(this)}>load tweets</button>
+    }
+
+    const mappedTweets = tweets.map(tweet => <li>{tweet.text}</li>)
+
+    return <div>
+      <h1>{user.name}</h1>
+      <ul>{mappedTweets}</ul>
+    </div>
+  }
+}
+
+*/
+
